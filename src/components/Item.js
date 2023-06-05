3bcd9f88f08c2b6b690e5d4ec66232fb1b4c2c71
Irvin Dinkins <irvdude44@gmail.com>
2023-06-05 00:24:30 -0400

    passed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [itemAddState, setItemAddState] = useState(false)
+
+  function addItem() {
+    setItemAddState(itemAddState === true ? false : true)
+  }
+
+  const itemClass = itemAddState ? "in-cart" : ""
+  const addItemText = itemAddState ? "Remove from Cart" : "Add to Cart"
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addItem}>{addItemText}</button>
     </li>
   );
 }
